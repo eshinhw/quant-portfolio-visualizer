2f3d3b76ab271f53f5e78c482ca1a829cd30a39e
Eddie Shin <eshinhw@gmail.com>
2020-09-15 00:18:43 -0400

    worked on questrade API

--- questrade.py
+++ questrade.py
@@ -6,15 +6,13 @@ from questrade_api import Questrade
 
 q = Questrade()
 
-
-
 def get_account_num():
     """
     Returns a list of account numbers associated with API key
     """
     
     accounts = []
-    print(q.accounts)
+    #print(q.accounts)
     
     for acct in q.accounts['accounts']:
         accounts.append(acct['number'])
@@ -34,17 +32,49 @@ def get_holding_symbols(acctNum):
     
     return symbols
 
-def get_open_positions(acctNum):
-    pass
+def get_open_positions(acctNum):    
+    
+    selectedKeys = ['symbol', 'symbolId', 'openQuantity', 'currentMarketValue', 'currentPrice']
+    
+    openPos = []
+    
+    for pos in q.account_positions(acctNum)['positions']:
+        posInfo = {}
+        for k,v in pos.items():
+            if k in selectedKeys:
+                posInfo[k] = v
+        
+        openPos.append(posInfo)
+        
+    
+    return openPos    
 
-def get_account_balance():
-    pass
+def get_CAD_account_balance(acctNum):
+    
+    selectedKeys = ['currency','cash', 'marketValue', 'totalEquity']
+    
+    
+    for bal in q.account_balances(acctNum)['perCurrencyBalances']:
+        
+        if bal['currency'] == 'CAD':    
+            cadBal = {}
+            for k,v in bal.items():
+                print(k,v)
+                if k in selectedKeys:
+                    cadBal[k] = v
+    
+    return cadBal
+                
+            
+    
+    
 
 
-
+#print(q.account_balances(get_account_num()[0])['perCurrencyBalances'])
 
     
     
 
-get_holding_symbols(get_account_num()[0])
+get_CAD_account_balance(get_account_num()[0])
 
+#print(q.account_positions(get_account_num()[0]))
