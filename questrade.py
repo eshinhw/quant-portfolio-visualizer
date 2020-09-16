6a344fdabbf5d3bdfb4a726bfd96a14b0730ecda
Eddie Shin <eshinhw@gmail.com>
2020-09-15 23:08:27 -0400

    working on rebalancing logic

--- questrade.py
+++ questrade.py
@@ -12,17 +12,16 @@ def get_account_num():
     """
     
     accounts = []
-    #print(q.accounts)
     
     for acct in q.accounts['accounts']:
         accounts.append(acct['number'])
     
     return accounts
 
-
 def get_holding_symbols(acctNum):
     """
     Returns a list of symbols of holding assets
+    ['VFV.TO', 'XBB.TO']
     """
     
     symbols = []
@@ -32,7 +31,13 @@ def get_holding_symbols(acctNum):
     
     return symbols
 
-def get_open_positions(acctNum):    
+def get_open_positions(acctNum): 
+    """
+    Returns a list of open positions with market info
+    [{'symbol': 'VFV.TO', 'symbolId': 2874671, 'openQuantity': 6, 'currentMarketValue': 477.6, 'currentPrice': 79.6}, 
+     {'symbol': 'XBB.TO', 'symbolId': 24003, 'openQuantity': 35, 'currentMarketValue': 1180.55, 'currentPrice': 33.73}]
+    
+    """
     
     selectedKeys = ['symbol', 'symbolId', 'openQuantity', 'currentMarketValue', 'currentPrice']
     
@@ -44,37 +49,39 @@ def get_open_positions(acctNum):
             if k in selectedKeys:
                 posInfo[k] = v
         
-        openPos.append(posInfo)
-        
+        openPos.append(posInfo)        
     
     return openPos    
 
-def get_CAD_account_balance(acctNum):
+
+def get_active_balance_cad(acctNum):
+    """
+    Returns a list of active balances in CAD
+    {'currency': 'CAD', 'cash': 87.5525, 'marketValue': 1658.87, 'totalEquity': 1746.4225}
+    """
     
-    selectedKeys = ['currency','cash', 'marketValue', 'totalEquity']
+    selectedKeys = ['currency','cash', 'marketValue', 'totalEquity']    
     
-    
-    for bal in q.account_balances(acctNum)['perCurrencyBalances']:
-        
-        if bal['currency'] == 'CAD':    
-            cadBal = {}
+    for bal in q.account_balances(acctNum)['perCurrencyBalances']:        
+        if bal['totalEquity'] != 0 and bal['currency'] == 'CAD': 
+            tempCAD = {}
             for k,v in bal.items():
-                print(k,v)
                 if k in selectedKeys:
-                    cadBal[k] = v
-    
-    return cadBal
-                
-            
-    
-    
+                    tempCAD[k] = v     
 
+            return tempCAD
 
-#print(q.account_balances(get_account_num()[0])['perCurrencyBalances'])
-
+def get_active_balance_usd(acctNum):
+    """
+    Returns a list of active balances in USD   
+    """
     
+    selectedKeys = ['currency','cash', 'marketValue', 'totalEquity']   
     
-
-get_CAD_account_balance(get_account_num()[0])
-
-#print(q.account_positions(get_account_num()[0]))
+    for bal in q.account_balances(acctNum)['perCurrencyBalances']:        
+        if bal['totalEquity'] != 0 and bal['currency'] == 'USD': 
+            tempUSD = {}
+            for k,v in bal.items():
+                if k in selectedKeys:
+                    tempUSD[k] = v
+            return tempUSD
--- rebalancing.py
+++ rebalancing.py
@@ -1,10 +1,83 @@
+import questrade as qt
+import pandas as pd
+from prettytable import PrettyTable
 
-import tkinter as tk
+# input target allocation ratio bewteen two assets
+target = {'VFV.TO': 0.8, 'XBB.TO': 0.2}
+# input transaction cost for selling assets
+transaction = 5
 
-root = tk.Tk()
+acctNum = qt.get_account_num()[0]
 
-button = tk.Button(root, text = "Hello")
-button.pack()
+totalEquity = qt.get_active_balance_cad(acctNum)['totalEquity']
+cash = qt.get_active_balance_cad(acctNum)['cash']
 
+positions = qt.get_open_positions(acctNum)
 
-root.mainloop()
+df = pd.DataFrame()
+
+# add positions into dataframe
+
+for position in positions:    
+    
+    symbol = position['symbol']
+    openQuantity = position['openQuantity']
+    currMV = position['currentMarketValue']
+    currPrice = position['currentPrice']    
+    t = target[position['symbol']]
+    
+    df = df.append(position, ignore_index=True)    
+
+df.set_index('symbol', inplace=True)
+
+# calculate quantity changes for rebalancing
+
+for symbol in df.index:
+    
+    df.loc[symbol,'targetValue'] = (totalEquity * target[symbol])
+    df.loc[symbol,'variation'] = df.loc[symbol,'targetValue'] - df.loc[symbol,'currentMarketValue']  
+    df.loc[symbol,'Qty Change'] = df.loc[symbol,'variation'] / df.loc[symbol,'currentPrice']
+    
+# change datatype for certain columns
+
+df = df.astype({'Qty Change': int, 'symbolId': int, 'openQuantity': int})
+
+# sort by qty change so that we can sell some first and buy other later with more cash
+
+df.sort_values(by=['Qty Change'], inplace=True)
+
+# iterate from the top to sell first and buy later with cash adjustment
+
+x = PrettyTable()
+
+x.field_names = ['SYMBOL', 'ACTION REQ.', '# SHARES', 'PRICE']
+
+for symbol in df.index:
+
+    qty = df.loc[symbol,'Qty Change']
+    currP = df.loc[symbol, 'currentPrice']
+    
+    # buying
+    if qty > 0:
+        buy = qty * currP
+        cash = cash - buy
+        df.loc[symbol, 'after rebalancing'] = df.loc[symbol, 'currentMarketValue'] + buy
+        x.add_row([symbol, 'BUY', int(qty), currP])
+        #print("Total ${} is subtracted from cash account".format(buy))
+    
+    # selling
+    elif qty < 0:
+        sell = (-qty) * currP
+        cash = cash + sell - transaction
+        df.loc[symbol, 'after rebalancing'] = df.loc[symbol, 'currentMarketValue'] - sell
+        x.add_row([symbol, 'SELL', -int(qty), currP])
+        #print("Total ${} is added from cash account".format(sell))
+    
+    # no rebalancing
+    else:
+        print("{} |  -  |      -     | $ -".format(symbol))
+
+        
+df = df.astype({'after rebalancing': float})
+print(x)
+print(cash)
