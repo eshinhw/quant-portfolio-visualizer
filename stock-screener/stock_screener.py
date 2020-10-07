7c6b2e9f1167afe597b057590d5214e4a4afdb5c
Eddie Shin <eshinhw@gmail.com>
2020-10-06 23:29:44 -0400

    Chowder rule and rule based investing brainstorming

--- stock-screener/stock_screener.py
+++ stock-screener/stock_screener.py
@@ -1,3 +1,10 @@
 import pandas as pd
 import numpy as np
 
+"""
+Chowder Rule - Dividend Growth Investing
+High total return = high quality + high yield + high growth of yield
+
+Quantitative and Systematic Rule Based Investing
+
+"""
