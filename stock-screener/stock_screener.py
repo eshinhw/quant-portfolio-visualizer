1d36c111a8e1d152075085f4d28df3b3bf3c4ff8
Eddie Shin <eshinhw@gmail.com>
2020-10-07 22:16:55 -0400

    Dividend Growth Investing Brainstorming

--- stock-screener/stock_screener.py
+++ stock-screener/stock_screener.py
@@ -7,4 +7,10 @@ High total return = high quality + high yield + high growth of yield
 
 Quantitative and Systematic Rule Based Investing
 
+Value + Growth + Quality + Dividend
+
+Finding high quality under valued stocks with margin of safety
+Make sure the selected stocks have high growth potential
+While continuously growing business models, the selected companies also respect their shareholders by paying dividends
+
 """
