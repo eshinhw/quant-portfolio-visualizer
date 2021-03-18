d91b33337b3f0f5d502422d8a797fed5f6a49e00
Eddie Shin <eshinhw@gmail.com>
2021-03-17 21:23:38 -0400

    working on dividend investing

--- questrade-portfolio-manager/aa.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-description_table = pd.DataFrame(columns=['Symbol', 'Weight', 'Symbol Description'])
-
-print(description_table)
--- questrade-portfolio-manager/main.py
+++ questrade-portfolio-manager/main.py
@@ -32,30 +32,30 @@ mpList = [balanced, emerging, international,crisis]
 mpName = [balancedName, emergingName, internationalName, crisisName]
 
 # User Interace Menu
-    
-while (1):     
+
+while (1):
     helper.print_header('MAIN MENU')
     print(helper.INDENT + '1. MODEL PORTFOLIO PERFORMANCE COMPARISON')
     print(helper.INDENT + '2. PORTFOLIO SELECTION FOR REBALANCING')
     print(helper.INDENT + '3. ASSET ALLOCATION PORTFOLIO BACKTESTING')
-    print()    
+    print()
     print(helper.INDENT + '0. Exit')
-    
-    action = input('>> ')    
-    
+
+    action = input('>> ')
+
     if action == '0':
         sys.exit()
-    
+
     elif action == '1':
         helper.print_header('MODEL PORTFOLIO PERFORMANCE COMPARISON')
         bt.performance_comparison(mpList, mpName)
         print()
         print('END OF DATA')
         print()
-        print("PRESS ANYTHING TO GO BACK TO MAIN MENU.")        
-        action = input('>> ')   
-        continue         
-    
+        print("PRESS ANYTHING TO GO BACK TO MAIN MENU.")
+        action = input('>> ')
+        continue
+
     if action == '2':
         helper.print_header('MODEL PORTFOLIO SELECTION')
         print(helper.INDENT + '1. ' + balancedName)
@@ -64,42 +64,42 @@ while (1):
         print(helper.INDENT + '4. ' + crisisName)
         print()
         print(helper.INDENT + '0. EXIT')
-    
+
         port_choice = input('>> ')
-        
+
         if port_choice == '0':
             sys.exit()
-        
+
         elif port_choice == '1':
             name = "SELECTED PORTFOLIO :: " + balancedName
             helper.print_header(name)
-            pt.get_description(balanced)    
+            pt.get_description(balanced)
         elif port_choice == '2':
             name = "SELECTED PORTFOLIO :: " + emergingName
             helper.print_header(name)
             pt.get_description(emerging)
         elif port_choice == '3':
-            name = "SELECTED PORTFOLIO :: " + internationalName    
+            name = "SELECTED PORTFOLIO :: " + internationalName
             helper.print_header(name)
             pt.get_description(international)
         elif port_choice == '4':
-            name = "SELECTED PORTFOLIO :: " + crisisName        
+            name = "SELECTED PORTFOLIO :: " + crisisName
             helper.print_header(name)
             pt.get_description(crisis)
-        
+
         print()
         print("PROCEED WITH SELECTED PORTFOLIO FOR REBALANCING? [Y/N]")
         proceed = input('>> ')
-        
-        if proceed == 'Y' or proceed == 'y':        
+
+        if proceed == 'Y' or proceed == 'y':
             allocation = {
                         '1': balanced,
                         '2': emerging,
                         '3': international,
-                        '4': crisis}            
-            rb.rebalancing(allocation[port_choice])           
-        
+                        '4': crisis}
+            rb.rebalancing(allocation[port_choice])
+
         else:
             continue
-    
-    
+
+
