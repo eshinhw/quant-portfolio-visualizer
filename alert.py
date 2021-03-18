e1f71c3283f12b62f6f56fe11f8b8910788763f3
Eddie Shin <eshinhw@gmail.com>
2021-03-18 11:02:37 -0400

    working on alert system

--- alert.py
+++ alert.py
@@ -3,18 +3,19 @@ import smtplib
 from email.message import EmailMessage
 
 EMAIL_ADDRESS = "eshinhw@gmail.com"
-EMAIL_PASSWORD = "qjsdfhgqdphqpbvd"
+EMAIL_PASSWORD = ""
 
-msg = EmailMessage()
+# msg = EmailMessage()
 
-msg['Subject'] = "Test 1"
-msg['From'] = "eshinhw@gmail.com"
-msg['To'] = "eshinhw@gmail.com"
+# msg['Subject'] = "Test 1"
+# msg['From'] = "eshinhw@gmail.com"
+# msg['To'] = "eshinhw@gmail.com"
 
-msg.set_content(rebal.get_html_string())
+#msg.set_content(rebal.get_html_string())
 
-# with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-#     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-#     smtp.send_message(msg)
-    
-#     print('completed')
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    smtp.sendmail("eshinhw@gmail.com", "eshinhw@gmail.com",
+                  "hello!")
+
+    print('completed')
