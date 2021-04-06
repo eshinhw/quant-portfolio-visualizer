b4700e98e243a5df2d44140a0f8fb6c73dd62438
Eddie Shin <eshinhw@gmail.com>
2021-04-06 18:04:45 -0400

    re-organized code structure

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import oandapyV20.endpoints.pricing as pricing
 import oandapyV20.endpoints.accounts as accounts
 import oandapyV20.endpoints.instruments as instruments
 
-# INSTRUMENTS = ["EUR_USD", "GBP_USD", "USD_JPY", "USD_CAD"]
-INSTRUMENTS = ["EUR_USD"]
+INSTRUMENTS = ["EUR_USD", "GBP_USD", 'AUD_USD', 'NZD_USD']
+#INSTRUMENTS = ["EUR_USD"]
 RISK_PER_TRADE = 0.001
 
 POSITION_STATUS = {}
@@ -68,7 +68,7 @@ def get_trade_list(accountID):
     """
     r = trades.TradesList(accountID)
     resp = client.request(r)
-    print("RESPONSE:\n{}".format(json.dumps(resp, indent=2)))
+    #print("RESPONSE:\n{}".format(json.dumps(resp, indent=2)))
     return resp["trades"]
 
 
@@ -207,6 +207,8 @@ def create_sell_limit(pair, entry, stop_loss, unit_size):
     r = orders.OrderCreate(accountID, data=order_body)
     client.request(r)
     POSITION_STATUS[pair] = 1
+    print(
+        f"LIMIT Trade Placed --- pair: {pair} | entry: {str(entry)} | stop_loss: {str(stop_loss)} | unit_size: {str(unit_size)}")
 
 
 def create_buy_limit(pair, entry, stop_loss, unit_size):
@@ -224,74 +226,98 @@ def create_buy_limit(pair, entry, stop_loss, unit_size):
     r = orders.OrderCreate(accountID, data=order_body)
     client.request(r)
     POSITION_STATUS[pair] = 1
+    print(
+        f"LIMIT Trade Placed --- pair: {pair} | entry: {str(entry)} | stop_loss: {str(stop_loss)} | unit_size: {str(unit_size)}")
 
 
-# Initialize OANDA API Client
-with open("oanda_demo_api_token.txt", "r") as secret:
-    contents = secret.readlines()
-    api_token = contents[0].rstrip("\n")
-    accountID = contents[1]
-    secret.close()
+# def create_buy_market(pair, stop_loss, unit_size):
+#     order_body = {
+#         "order": {
+#             "price": str(entry),
+#             "stopLossOnFill": {"timeInForce": "GFD", "price": str(stop_loss)},
+#             "timeInForce": "GFD",
+#             "instrument": pair,
+#             "units": str(unit_size),
+#             "type": "MARKET",
+#             "positionFill": "DEFAULT",
+#         }
+#     }
+#     r = orders.OrderCreate(accountID, data=order_body)
+#     client.request(r)
+#     POSITION_STATUS[pair] = 1
 
-client = API(access_token=api_token)
 
-# cancel_all_trades(accountID)
-# get_trade_list(accountID)
+# def create_sell_market(pair, stop_loss, unit_size):
+#     order_body = {
+#         "order": {
+#             "price": str(entry),
+#             "stopLossOnFill": {"timeInForce": "GFD", "price": str(stop_loss)},
+#             "timeInForce": "GFD",
+#             "instrument": pair,
+#             "units": str(unit_size),
+#             "type": "MARKET",
+#             "positionFill": "DEFAULT",
+#         }
+#     }
+#     r = orders.OrderCreate(accountID, data=order_body)
+#     client.request(r)
+#     POSITION_STATUS[pair] = 1
 
+def execute(accountID):
+    # get two previous 4H candles & check ohlc -> c0, c1
+    for pair in INSTRUMENTS:
+        update_position_status(pair, accountID)
 
-# get two previous 4H candles & check ohlc -> c0, c1
-for pair in INSTRUMENTS:
-    update_position_status(pair, accountID)
+        q = 0.95
+        rk = 1.5
+        entry_buffer = 0.0003
 
-    q = 0.95
-    rk = 1.5
+        candles = get_candle_data(pair, 3, "H4")["candles"]
+        base = candles[0]
+        signal = candles[1]
+        current = candles[2]
 
-    candles = get_candle_data(pair, 3, "H4")["candles"]
-    base = candles[0]
-    signal = candles[1]
-    current = candles[2]
+        signal_close = float(signal["mid"]["c"])
+        signal_range = float(signal["mid"]["h"]) - float(signal["mid"]["l"])
+        signal_upper_close_threshold = float(
+            signal["mid"]["l"]) + (signal_range * q)
+        signal_lower_close_threshold = float(
+            signal["mid"]["h"]) - (signal_range * q)
 
-    signal_close = float(signal["mid"]["c"])
-    signal_range = float(signal["mid"]["h"]) - float(signal["mid"]["l"])
-    signal_upper_close_threshold = float(
-        signal["mid"]["l"]) + (signal_range * q)
-    signal_lower_close_threshold = float(
-        signal["mid"]["h"]) - (signal_range * q)
+        base_high = float(base["mid"]["h"])
+        base_low = float(base["mid"]["l"])
+        base_range = float(base["mid"]["h"]) - float(base["mid"]["l"])
+        # print(signal_upper_close_threshold)
+        # print(signal_range)
+        # print(base_range)
 
-    base_high = float(base["mid"]["h"])
-    base_low = float(base["mid"]["l"])
-    base_range = float(base["mid"]["h"]) - float(base["mid"]["l"])
-    print(signal_upper_close_threshold)
-    print(signal_range)
-    print(base_range)
+        # buy setup
+        if (
+            signal_close > base_high
+            and signal_close > signal_upper_close_threshold
+            and signal_range > (base_range * rk)
+        ):
+            if POSITION_STATUS[pair] == 0:
+                entry = float(current["mid"]["o"]) - entry_buffer
+                stop_loss = float(signal["mid"]["l"])
+                unit_size = calculate_unit_size(entry, stop_loss)
+                create_buy_limit(pair, entry, stop_loss, unit_size)
+            else:
+                continue
 
-    # buy setup
-    if (
-        signal_close > base_high
-        and signal_close > signal_upper_close_threshold
-        and signal_range > (base_range * rk)
-    ):
-        if POSITION_STATUS[pair] == 0:
-            entry = float(current["mid"]["o"])
-            stop_loss = float(signal["mid"]["l"])
-            unit_size = calculate_unit_size(entry, stop_loss)
-            create_buy_limit(pair, entry, stop_loss, unit_size)
-        else:
-            continue
-
-    # sell setup
-    if (
-        signal_close < base_low
-        and signal_close < signal_lower_close_threshold
-        and signal_range > (base_range * rk)
-    ):
-        if POSITION_STATUS[pair] == 0:
-            entry = float(current["mid"]["o"])
-            stop_loss = float(signal["mid"]["l"])
-            unit_size = calculate_unit_size(entry, stop_loss)
-            create_sell_limit(pair, entry, stop_loss, unit_size)
-        else:
-            continue
+        # sell setup
+        if (
+            signal_close < base_low
+            and signal_close < signal_lower_close_threshold
+            and signal_range > (base_range * rk)
+        ):
+            if POSITION_STATUS[pair] == 0:
+                entry = float(current["mid"]["o"]) + entry_buffer
+                stop_loss = float(signal["mid"]["l"])
+                unit_size = calculate_unit_size(entry, stop_loss)
+                create_sell_limit(pair, entry, stop_loss, unit_size)
+            else:
+                continue
     # if that's true: buy at current open + stop loss at previous candle's high or low
 
     # when the current candle is complete, move stop to current candle's high or low
@@ -335,3 +361,16 @@ for pair in INSTRUMENTS:
 
     # print(f'buy stop: {buy_stop}, sell stop: {sell_stop}')
     # print(f'stop loss: {stop_loss}, take profit: {take_profit}')
+
+
+if __name__ == '__main__':
+
+    with open("oanda_demo_api_token.txt", "r") as secret:
+        contents = secret.readlines()
+        api_token = contents[0].rstrip("\n")
+        accountID = contents[1]
+        secret.close()
+
+    client = API(access_token=api_token)
+
+    execute(accountID)
