fc4208cf71cfb2acae9a6eb0f9d69be386b7d313
Eddie Shin <eshinhw@gmail.com>
2021-04-13 10:17:28 -0400

    updated structure

--- turtle_soup.py
+++ turtle_soup.py
@@ -2,6 +2,7 @@ import time
 import oanda
 import schedule
 import pandas as pd
+import dfdata
 
 # SYMBOLS = ["EUR_USD"]
 #SYMBOLS = ["EUR_USD", "GBP_USD", "AUD_USD", 'NZD_USD', 'USD_JPY', 'GBP_JPY']
@@ -22,111 +23,51 @@ ATR_SL_MULTIPLE = 1
 account_ID = '101-002-5334779-003'
 
 
-def retrieve_data(symbol, days):
-    candles = oanda.get_candle_data(symbol, days + 1, "D")
+def check_condition_and_place_orders(symbol: str):
 
-    data_dict = {"Date": [], "Open": [], "High": [], "Low": [], "Close": []}
-
-    for candle in candles["candles"]:
-        data_dict["Date"].append(candle["time"][: candle["time"].index("T")])
-        data_dict["Open"].append(float(candle["mid"]["o"]))
-        data_dict["High"].append(float(candle["mid"]["h"]))
-        data_dict["Low"].append(float(candle["mid"]["l"]))
-        data_dict["Close"].append(float(candle["mid"]["c"]))
-
-    df = pd.DataFrame(data_dict)
-    df.set_index("Date", inplace=True)
-    return df
-
-
-def calculate_ATR(df, symbol, days):
-    df = df.copy()
-    # https://stackoverflow.com/questions/40256338/calculating-average-true-range-atr-on-ohlc-data-with-python
-    # https://stackoverflow.com/questions/35753914/calculating-average-true-range-column-in-pandas-dataframe
-    df["RangeOne"] = abs(df["High"] - df["Low"])
-    df["RangeTwo"] = abs(df["High"] - df["Close"].shift())
-    df["RangeThree"] = abs(df["Close"].shift() - df["Low"])
-    df["TrueRange"] = df[["RangeOne", "RangeTwo", "RangeThree"]].max(axis=1)
-    df["ATR"] = df["TrueRange"].ewm(span=days).mean()
-    return df["ATR"].iloc[-1]
-
-
-def long_entry(df, symbol, days):
-    df = df.copy()
-    df["High_" + str(days)] = df["High"].shift(1).rolling(window=days).max()
-
-    return df["High_" + str(days)].iloc[-1]
-
-
-def short_entry(df, symbol, days):
-    df = df.copy()
-    df["Low_" + str(days)] = df["Low"].shift(1).rolling(window=days).min()
-
-    return df["Low_" + str(days)].iloc[-1]
-
-
-def prev_low(df, symbol, days):  # place to go long
-    df = df.copy()
-    df["Low_" + str(days)] = df["Low"].shift(1).rolling(window=days).min()
-
-    return df["Low_" + str(days)].iloc[-1]
-
-
-def prev_high(df, symbol, days):  # place to go short
-    df = df.copy()
-    df["High_" + str(days)] = df["High"].shift(1).rolling(window=days).max()
-    return df["High_" + str(days)].iloc[-1]
-
-
-def place_orders(symbol: str):
-
-    decimal = 5
-
-    if "_USD" in symbol:
-        decimal = 5
-    if "_JPY" in symbol:
-        decimal = 3
-
-    # retrieve price data
-    df = retrieve_data(symbol, PREV_DAYS)
-    # entry prices
-    long_entry_price = round(
-        prev_low(df, symbol, PREV_DAYS) + ENTRY_PIP_BUFF, decimal)
-    short_entry_price = round(
-        prev_high(df, symbol, PREV_DAYS) - ENTRY_PIP_BUFF, decimal)
-
-    stop_loss = calculate_ATR(df, symbol, PREV_DAYS) * ATR_SL_MULTIPLE
-    long_sl = round(long_entry_price - stop_loss, decimal)
-    short_sl = round(short_entry_price + stop_loss, decimal)
-
-    if (not oanda.check_open_order(account_ID, symbol)) and (not oanda.check_open_trade(account_ID, symbol)):
-        oanda.create_buy_limit(
-            account_ID,
-            symbol,
-            long_entry_price,
-            long_sl,
-            oanda.calculate_unit_size(
-                account_ID, symbol, long_entry_price, long_sl, RISK_PER_TRADE),
-            True,
-        )
-
-        oanda.create_sell_limit(
-            account_ID,
-            symbol,
-            short_entry_price,
-            short_sl,
-            oanda.calculate_unit_size(account_ID, symbol,
-                                      short_entry_price, short_sl, RISK_PER_TRADE),
-            True,
-        )
-
-
-def check_trade_conditions():
     for symbol in SYMBOLS:
-        place_orders(symbol)
+
+        decimal = 5
+
+        if "_USD" in symbol:
+            decimal = 5
+        if "_JPY" in symbol:
+            decimal = 3
+
+        # entry prices
+        long_entry_price = round(
+            dfdata.calculate_prev_min_low(symbol, PREV_DAYS, 'D') + ENTRY_PIP_BUFF, decimal)
+        short_entry_price = round(
+            dfdata.calculate_prev_max_high(symbol, PREV_DAYS, 'D') - ENTRY_PIP_BUFF, decimal)
+
+        stop_loss = dfdata.calculate_ATR(
+            symbol, PREV_DAYS, 'D') * ATR_SL_MULTIPLE
+        long_sl = round(long_entry_price - stop_loss, decimal)
+        short_sl = round(short_entry_price + stop_loss, decimal)
+
+        if (not oanda.check_open_order(account_ID, symbol)) and (not oanda.check_open_trade(account_ID, symbol)):
+            oanda.create_buy_limit(
+                account_ID=account_ID,
+                symbol=symbol,
+                entry=long_entry_price,
+                stop=long_sl,
+                units=oanda.calculate_unit_size(
+                    account_ID, symbol, long_entry_price, long_sl, RISK_PER_TRADE),
+                trailing_stop=True,
+            )
+
+            oanda.create_sell_limit(
+                account_ID=account_ID,
+                symbol=symbol,
+                entry=short_entry_price,
+                stop=short_sl,
+                units=oanda.calculate_unit_size(account_ID, symbol,
+                                                short_entry_price, short_sl, RISK_PER_TRADE),
+                trailing_stop=True,
+            )
 
 
 if __name__ == "__main__":
 
     oanda.update_order_trade_status(account_ID)
-    check_trade_conditions()
+    check_condition_and_place_orders()
