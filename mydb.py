f0fa64b297700c60eed9d072a1ae5b258c39dc1a
Eddie Shin <eshinhw@gmail.com>
2021-04-22 18:31:13 -0400

    finished funcs collecting ratio and price data into db

--- mydb.py
+++ mydb.py
@@ -1,3 +1,5 @@
+import os
+import json
 import requests
 import sqlalchemy
 import pandas as pd
@@ -46,15 +48,16 @@ class db_master:
             passwd = pw
         )
         self.symbol = symbol.upper()
+        print(self.symbol)
         self.mycursor = self.db.cursor()
-        self.mycursor.execute(f"CREATE DATABASE IF NOT EXISTS {symbol}")
+        self.mycursor.execute(f"CREATE DATABASE IF NOT EXISTS {self.symbol}")
         self.db.commit()
 
         creds = {'usr': user_id,
         'pwd': pw,
         'hst': host_ip,
         'prt': 3306,
-        'dbn': symbol}
+        'dbn': self.symbol}
         connstr = 'mysql+mysqlconnector://{usr}:{pwd}@{hst}:{prt}/{dbn}'
         self.engine = sqlalchemy.create_engine(connstr.format(**creds))
 
@@ -71,33 +74,47 @@ class db_master:
         return None
 
     def financial_ratios(self, limit: int):
+        self.mycursor.execute(f"USE {self.symbol}")
+        self.mycursor.execute("CREATE TABLE IF NOT EXISTS Financial_Ratios (Date VARCHAR(100) NOT NULL, Ratio VARCHAR(255) NOT NULL, Value float)")
+        self.db.commit()
 
         # data = {'Symbol': [], 'Date': [], 'Revenue_Growth': [], 'ROE': [], 'Dividend_Yield': [], 'Payout Ratio': []}
 
-        financial_ratios = requests.get(f'https://financialmodelingprep.com/api/v3/ratios-ttm/{self.symbol}?apikey={self.key}').json()
+        financial_growth = requests.get(f'https://financialmodelingprep.com/api/v3/financial-growth/{self.symbol}?period=quarter&limit={limit}&apikey={self.key}').json()[0]
+        financial_ratios = requests.get(f'https://financialmodelingprep.com/api/v3/ratios-ttm/{self.symbol}?apikey={self.key}').json()[0]
 
-        return financial_ratios
+        vals = []
+        date = financial_growth.get('date')
 
-    def financial_growth(self, key: str, limit: int):
+        for k in financial_growth.keys():
+            if k in ('symbol', 'period', 'date'):
+                continue
+            else:
+                val = financial_growth.get(k)
+                vals.append((date,k,val))
 
-        financial_growth = requests.get(f'https://financialmodelingprep.com/api/v3/financial-growth/{self.symbol}?period=quarter&limit={limit}&apikey={self.key}').json()
+        for k in financial_ratios.keys():
+            val = financial_ratios.get(k)
+            vals.append((date,k,val))
 
-        return financial_growth
+        self.mycursor.executemany("INSERT INTO Financial_Ratios (Date, Ratio, Value) VALUES (%s,%s,%s)", vals)
+        self.db.commit()
 
     def price_df_to_sql(self):
         try:
             prices = yf.Ticker(self.symbol).history(period='max')
         except:
             return None
+
         prices.reset_index(inplace=True)
 
-        self.mycursor.execute(f"CREATE TABLE IF NOT EXISTS {self.symbol} (Date datetime, Open float, High float, Low float, Close float, Dividends float, Stock_Splits float)")
+        self.mycursor.execute(f"CREATE TABLE IF NOT EXISTS Price (Date datetime, Open float, High float, Low float, Close float, Dividends float, Stock_Splits float)")
         self.db.commit()
 
-        prices.to_sql(name=self.symbol, con=self.engine, if_exists='replace', index=False)
+        prices.to_sql(name='Price', con=self.engine, if_exists='replace', index=False)
 
     def price_sql_to_df(self):
-        self.mycursor.execute(f"SELECT * FROM {self.symbol}")
+        self.mycursor.execute(f"SELECT * FROM Price")
 
         df = pd.DataFrame(self.mycursor.fetchall(), columns=['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Dividends', 'Stock_Splits'])
         df.set_index('Date',inplace=True)
@@ -105,7 +122,22 @@ class db_master:
 
 if __name__ == '__main__':
 
-    aapl = db_master('AAPL', fmp_api)
-    x = aapl.income_statement('Q', 8)
-    print(x)
+    # if os.path.exists('./apple_data.json'):
+    #     with open('./apple_data.json', 'r') as fp:
+    #         x = json.load(fp)
+    # else:
+    #     aapl = db_master('AAPL', fmp_api)
+    #     x = aapl.financial_ratios(5)
+    #     with open('./apple_data.json', 'w') as fp:
+    #         json.dump(x, fp)
+
+    # x = x[0]
+    # for k in x.keys():
+    #     val = x.get(k)
+
+    aapl = db_master('aapl', fmp_api)
+    aapl.financial_ratios(20)
+    aapl.price_df_to_sql()
+
+    # abc = db_master('abc', fmp_api)
 
