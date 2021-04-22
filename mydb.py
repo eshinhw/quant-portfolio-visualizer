e71eb340a154900764a974754a36dc9cfe880f2a
Eddie Shin <eshinhw@gmail.com>
2021-04-22 03:20:58 -0400

    set up remote database

--- mydb.py
+++ mydb.py
@@ -1,5 +1,9 @@
 import mysql.connector
-from datetime import datetime
+import datetime as dt
+import yfinance as yf
+import pandas_datareader.data as web
+import sqlalchemy
+import pandas as pd
 
 with open('./credentials.txt', 'r') as fp:
     secret = fp.readlines()
@@ -11,13 +15,58 @@ with open('./credentials.txt', 'r') as fp:
 db = mysql.connector.connect(
     host = host_ip,
     user = user_id,
-    passwd = pw
+    passwd = pw,
+    db = 'financial_data'
 )
 
 mycursor = db.cursor()
-mycursor.execute("SHOW DATABASES")
-for x in mycursor:
-    print(x)
+# mycursor.execute("SHOW DATABASES")
+# for x in mycursor:
+#     print(x)
+
+# engine=sqlalchemy.create_engine('mysql://')
+start_date = (dt.date.today() - dt.timedelta(days=5)).strftime("%Y-%m-%d")
+end_date = dt.date.today().strftime("%Y-%m-%d")
+
+creds = {'usr': user_id,
+            'pwd': pw,
+            'hst': host_ip,
+            'prt': 3306,
+            'dbn': 'financial_data'}
+# MySQL conection string.
+connstr = 'mysql+mysqlconnector://{usr}:{pwd}@{hst}:{prt}/{dbn}'
+# Create sqlalchemy engine for MySQL connection.
+engine = sqlalchemy.create_engine(connstr.format(**creds))
+
+#price_data = web.DataReader('MMM', 'yahoo', start_date, end_date)
+
+# print(prices)
+
+# print(type(prices['Open'].iloc[-1]))
+symbol = 'MMM'
+# mycursor.execute('USE financial_data')
+# mycursor.execute("CREATE TABLE MMM (Date datetime, Open float, High float, Low float, Close float, Volume int, Dividends float)")
+
+# db.commit()
+
+mycursor.execute("SELECT * FROM MMM")
+
+# print(mycursor.fetchall())
+
+new = pd.DataFrame(mycursor.fetchall(), columns=['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Dividends'])
+
+print(new)
+# mycursor.execute("SHOW TABLES")
+# for x in mycursor:
+#     print(x)
+# prices = yf.Ticker(symbol).history(period='max')
+# prices.reset_index(inplace=True)
+
+# prices.to_sql(name='MMM', con=engine, if_exists='replace', index=False)
+
+# for i, row in prices.iterrows():
+#     print(i,tuple(row))
+
 #mycursor.execute("CREATE TABLE Person (name VARCHAR(255), age smallint UNSIGNED, personID int PRIMARY KEY AUTO_INCREMENT)")
 #mycursor.execute("DESCRIBE Person")
 # mycursor.execute("INSERT INTO Person (name, age) VALUES (%s,%s)", ("Joe", 22))
