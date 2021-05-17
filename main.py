a53d01a7136c50a0119aa159bba4b51b46c3e209
Eddie Shin <eshinhw@gmail.com>
2021-05-17 01:22:47 -0400

    updated historical momentum func

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import os
+import json
 import secret
 import requests
+import pandas as pd
+import datetime as dt
 from typing import List, Dict
-
+import pandas_datareader.data as web
 from pytickersymbols import PyTickerSymbols
+
+START_DATE = dt.datetime(1970,1,1)
+END_DATE = dt.datetime.today()
+MOMENTUM_PERIOD = [12,36,60]
 
 """
 GET list of S&P 500 symbols (financialmodelingprep API)
@@ -16,10 +24,14 @@ def get_sp500_symbols(index: str) -> Dict:
     except:
         print('index not valid')
         return
-
+    #print(sp500[0]['industries'][0])
     for record in sp500:
+        # print(record)
         symbol = record['symbol']
-        sector = record['industries'][0]
+        try:
+            sector = record['industries'][0]
+        except:
+            sector = 'N/A'
         name = record['name']
         data[symbol] = {}
         data[symbol]['name'] = name
@@ -28,33 +40,37 @@ def get_sp500_symbols(index: str) -> Dict:
     return data
 
 
-
-api = secret.FMP_API_KEYS
-
-sp500 = requests.get(f'https://financialmodelingprep.com/api/v3/sp500_constituent?apikey={api}').json()
-
-
-
-
 """
 CALCULATE historical momentum (yahoo finance)
 1. calculate historical monthly prices for each stock
-2. momentum: Average (1,3,5,10)
+2. momentum: Average of 12, 36, 60 months
 """
 
-# def calculate_equal_weight_momentum(symbol: str, periods: List[int], start_date=None, end_date=None):
-#     # start_date = dt.datetime(1970,1,1)
-#     # end_date = dt.datetime.today()
-#     ret = []
-#     monthly_prices = get_historical_monthly_prices(symbol)
-#     for period in periods:
-#         #print(period)
-#         monthly_returns = monthly_prices.apply(lambda x: x/x.shift(period) - 1, axis=0)
-#         monthly_returns = monthly_returns.rename(columns={'Adj_Close': 'Returns'})
-#         #print(monthly_returns['Returns'].iloc[-1])
-#         ret.append(monthly_returns['Returns'].iloc[-1])
-#     #print(ret)
-#     return sum(ret) / len(ret)
+def get_prices(symbol, start_date=None, end_date=None):
+    return web.DataReader(symbol, 'yahoo', start_date, end_date)
+
+def get_historical_monthly_prices(symbol: str, start_date=None, end_date=None):
+    prices = web.DataReader(symbol, 'yahoo', start_date, end_date)
+    prices.dropna(inplace=True)
+    prices.reset_index(inplace=True)
+    prices = prices[['Date', 'Adj Close']]
+    prices['STD_YM'] = prices['Date'].map(lambda x : dt.datetime.strftime(x, '%Y-%m'))
+    month_list = prices['STD_YM'].unique()
+    monthly_prices = pd.DataFrame()
+    for m in month_list:
+        monthly_prices = monthly_prices.append(prices[prices['STD_YM'] == m].iloc[-1,:])
+    monthly_prices = monthly_prices.drop(columns=['STD_YM'], axis=1)
+    monthly_prices.set_index('Date', inplace=True)
+    return monthly_prices[:-1]
+
+def calculate_equal_weight_momentum(symbol: str, periods: List[int], start_date=None, end_date=None):
+    ret = []
+    monthly_prices = get_historical_monthly_prices(symbol, start_date, end_date)
+    for period in periods:
+        monthly_returns = monthly_prices.apply(lambda x: x/x.shift(period) - 1, axis=0)
+        monthly_returns = monthly_returns.rename(columns={'Adj Close': 'Returns'})
+        ret.append(monthly_returns['Returns'].iloc[-1])
+    return sum(ret) / len(ret)
 
 """
 GET dividend data (financialmodelingprep API)
@@ -62,3 +78,39 @@ GET dividend data (financialmodelingprep API)
 2. Dividend Grwoth: Average (3, 5)
 3. Dividend Payout
 """
+
+# api = secret.FMP_API_KEYS
+
+# sp500 = requests.get(f'https://financialmodelingprep.com/api/v3/sp500_constituent?apikey={api}').json()
+
+if __name__ == '__main__':
+    #print(get_prices('SSE'))
+
+    # print(get_historical_monthly_prices('MMM', START_DATE, END_DATE))
+
+    # print(calculate_equal_weight_momentum('AAPL', MOMENTUM_PERIOD, START_DATE, END_DATE))
+    if os.path.exists('./sp500_data.json'):
+        with open('./sp500_data.json', 'r') as fp:
+            sp500 = json.load(fp)
+
+    else:
+        sp500 = get_sp500_symbols('S&P 500')
+        sp500_symbols = list(sp500.keys())
+
+        # Historical Momentum
+        for symbol in sp500_symbols:
+            print(symbol)
+            try:
+                sp500[symbol]['momentum'] = calculate_equal_weight_momentum(symbol, MOMENTUM_PERIOD, START_DATE, END_DATE)
+            except KeyError as e:
+                print(e)
+                sp500[symbol]['momentum'] = -1
+
+        with open('./sp500_data.json', 'w') as fp:
+            json.dump(sp500, fp)
+
+
+
+
+
+
