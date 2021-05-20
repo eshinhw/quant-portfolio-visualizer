53cdda1e9f15eef1eb20d1aae472ae186234a8f2
Eddie Shin <eshinhw@gmail.com>
2021-05-19 22:49:32 -0400

    updated

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ def get_market_cap(symbol: str):
 
     market_cap = requests.get(f"https://financialmodelingprep.com/api/v3/market-capitalization/{symbol.upper()}?apikey={FMP_API}").json()
     print(market_cap)
-
+    print()
     return market_cap
 
 
