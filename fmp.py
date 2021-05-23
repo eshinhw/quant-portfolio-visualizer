9d8f3937cb18b354a6fd1ec9e0bb5b7cbaf497e0
Eddie Shin <eshinhw@gmail.com>
2021-05-22 22:45:45 -0400

    updated

--- fmp.py
+++ fmp.py
@@ -9,17 +9,31 @@ DB_PW = secret.DB_PASSWORDD
 
 
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password=DB_PW
+    host="localhost",
+    user="root",
+    password=DB_PW,
+    database="fmp"
 )
 
-mycursor = mydb.cursor()
+# mycursor = mydb.cursor()
 
-mycursor.execute("CREATE DATABASE mydatabase")
+# mycursor.execute("CREATE DATABASE IF NOT EXISTS fmp")
+
+# mycursor.execute("USE fmp")
+
+# mycursor.execute("CREATE TABLE IF NOT EXISTS company_profile (name VARCHAR(255), symbol VARCHAR(20), exchange VARCHAR(255), sector VARCHAR(255), industry VARCHAR(255), marketCap int)")
 
 
-# def company_profile():
+# mycursor.execute("ALTER TABLE company_profile ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
 
-#     data =
+def company_profile(symbol: str):
+
+    data = requests.get(f"https://financialmodelingprep.com/api/v3/profile/{symbol}?apikey={API_KEY}").json()
+    print(data)
+
+
+if __name__ == '__main__':
+
+    company_profile('aapl')
+
 
