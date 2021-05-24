b122d24101fc94cc110f9306a9f7eb7cbb59cbc8
Eddie Shin <eshinhw@gmail.com>
2021-05-23 21:47:05 -0400

    finished ratios func

--- fmp.py
+++ fmp.py
@@ -1,4 +1,5 @@
 import os
+import math
 import json
 import secret
 import pprint
@@ -8,6 +9,7 @@ import mysql.connector
 
 FMP_API_KEY = secret.FMP_API_KEYS
 DB_PW = secret.DB_PASSWORDD
+NaN = -9999999
 
 class fmp:
     def __init__(self) -> None:
@@ -21,7 +23,7 @@ class fmp:
         self.mycursor.execute("CREATE DATABASE IF NOT EXISTS fmp")
         self.mycursor.execute("USE fmp")
 
-    def sp500_symbol_list(self):
+    def load_sp500_symbol_list(self):
 
         if os.path.exists('./sp500_symbols.json'):
             fp = open("./sp500_symbols.json", "r")
@@ -52,10 +54,10 @@ class fmp:
         exchange VARCHAR(255), \
         sector VARCHAR(255), \
         industry VARCHAR(255), \
-        marketCap int)""")
+        marketCap float,
+        numEmployees int)""")
 
         data = requests.get(f"https://financialmodelingprep.com/api/v3/profile/{symbol.upper()}?apikey={FMP_API_KEY}").json()[0]
-        #pprint.pprint(data)
 
         name = data['companyName']
         symbol = data['symbol']
@@ -74,74 +76,50 @@ class fmp:
         self.mycursor.execute(sql,val)
         self.mydb.commit()
 
-    def ratio_ttm(self, symbol: str):
-        self.mycursor.execute("""CREATE TABLE IF NOT EXISTS ratio_ttm (\
+    def ratios(self, symbol: str):
+        self.mycursor.execute("""CREATE TABLE IF NOT EXISTS ratios (\
             id INT AUTO_INCREMENT PRIMARY KEY,
             symbol VARCHAR(255),
-            div_yield float,
-            curr_ratio float,
-            div_per_share float,
+            revenue_per_share_fiveY_growth float,
             gross_profit_margin float,
-            per float,
-            roe float)""")
+            roe float,
+            eps_growth float,
+            div_yield float,
+            div_per_share float,
+            dps_fiveY_growth float)""")
 
-        data = requests.get(f"https://financialmodelingprep.com/api/v3/ratios-ttm/{symbol}?apikey={FMP_API_KEY}").json()[0]
-        #pprint.pprint(data)
+        ratio_ttm = requests.get(f"https://financialmodelingprep.com/api/v3/ratios-ttm/{symbol.upper()}?apikey={FMP_API_KEY}").json()[0]
 
-        div_yield = data['dividendYieldTTM']
-        curr_ratio = data['currentRatioTTM']
-        div_per_share = data['dividendPerShareTTM']
-        gross_profit_margin = data['grossProfitMarginTTM']
-        per = data['priceEarningsRatioTTM']
-        roe = data['returnOnEquityTTM']
+        div_yield = ratio_ttm['dividendYieldTTM']
+        if not div_yield: return
+        div_per_share = ratio_ttm['dividendPerShareTTM']
+        if not div_per_share: return
+        gross_profit_margin = ratio_ttm['grossProfitMarginTTM']
+        roe = ratio_ttm['returnOnEquityTTM']
+
+        growth = requests.get(f"https://financialmodelingprep.com/api/v3/financial-growth/{symbol.upper()}?period=quarter&limit=20&apikey={FMP_API_KEY}").json()[0]
+
+        eps_growth = growth['epsgrowth']
+        if eps_growth <= 0: return
+        dps_fiveY_growth = growth['fiveYDividendperShareGrowthPerShare']
+        if dps_fiveY_growth <= 0: return
+        revenue_per_share_fiveY_growth = growth['fiveYRevenueGrowthPerShare']
+        if revenue_per_share_fiveY_growth <= 0: return
 
         sql = """
-        INSERT INTO ratio_ttm \
-        (symbol, div_yield, curr_ratio, div_per_share, gross_profit_margin, per, roe) \
-        VALUES (%s, %s, %s, %s, %s, %s, %s)"""
+        INSERT INTO ratios \
+        (symbol, revenue_per_share_fiveY_growth, gross_profit_margin, roe, eps_growth, div_yield, div_per_share, dps_fiveY_growth) \
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
 
         val = (
             symbol,
-            float(div_yield),
-            float(curr_ratio),
-            float(div_per_share),
+            float(revenue_per_share_fiveY_growth),
             float(gross_profit_margin),
-            float(per),
-            float(roe))
-
-        self.mycursor.execute(sql,val)
-        self.mydb.commit()
-
-    def growth(self, symbol: str):
-
-        data = requests.get(f"https://financialmodelingprep.com/api/v3/financial-growth/{symbol.upper()}?period=quarter&limit=20&apikey={FMP_API_KEY}").json()[0]
-
-        pprint.pprint(data)
-        self.mycursor.execute("""CREATE TABLE IF NOT EXISTS growth (\
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            symbol VARCHAR(255),
-            date VARCHAR(255),
-            gross_profit_margin float,
-            eps_growth float,
-            fcf_growth float,
-            dps_fiveY_growth float,
-            netIncome_per_share_fiveY_growth float,
-            revenue_per_share_fiveY_growth float)""")
-
-        sql = """
-        INSERT INTO growth \
-        (symbol, date, gross_profit_margin, eps_growth, fcf_growth, dps_fiveY_growth, netIncome_per_share_fiveY_growth, revenue_per_share_fiveY_growth) \
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-        """
-        date = data['date']
-        gross_profit_margin = data['grossProfitGrowth']
-        eps_growth = data['epsgrowth']
-        fcf_growth = data['freeCashFlowGrowth']
-        dps_fiveY_growth = data['fiveYDividendperShareGrowthPerShare']
-        netIncome_per_share_fiveY_growth = data['fiveYNetIncomeGrowthPerShare']
-        revenue_per_share_fiveY_growth = data['fiveYRevenueGrowthPerShare']
-
-        val = (symbol, date, float(gross_profit_margin), float(eps_growth), float(fcf_growth), float(dps_fiveY_growth), float(netIncome_per_share_fiveY_growth), float(revenue_per_share_fiveY_growth))
+            float(roe),
+            float(eps_growth),
+            float(div_yield),
+            float(div_per_share),
+            float(dps_fiveY_growth))
 
         self.mycursor.execute(sql,val)
         self.mydb.commit()
@@ -173,17 +151,21 @@ class fmp:
 
 if __name__ == '__main__':
 
+    # myfmp = fmp()
+
+    # myfmp.drop_all_databases()
+
     myfmp = fmp()
 
-    myfmp.drop_all_databases()
-
-    myfmp = fmp()
-
-    symbols = myfmp.sp500_symbol_list()
+    symbols = myfmp.load_sp500_symbol_list()
 
     for symbol in symbols:
+        myfmp.company_profile(symbol)
+        print(f"{symbol}: profile completed")
+        myfmp.ratios(symbol)
+        print(f"{symbol}: ratio completed")
 
-        print(symbol)
+
 
 
 
