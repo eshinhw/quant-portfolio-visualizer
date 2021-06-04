116e201e763d6dc060c7424fb76f037ceb838c0e
Eddie Shin <eshinhw@gmail.com>
2021-06-03 20:52:15 -0400

    updated allocation func

--- allocation.py
+++ allocation.py
@@ -12,4 +12,33 @@ qbot = questrade.qbot()
 
 # print(positions)
 
-print(qbot.get_ticker_info('TLT'))
+#print(qbot.get_ticker_info('TLT'))
+
+# total equity (market value + cash)
+
+# current market value (invested asset)
+
+# cash portion
+
+# stock vs cash weight
+
+stock_weight = 0.7
+numStocks = 15
+
+usd_equity = qbot.get_usd_total_equity()
+
+
+
+amount_per_stock = (usd_equity * stock_weight) / numStocks
+print(amount_per_stock)
+print(amount_per_stock * numStocks)
+
+print(qbot.get_positions())
+
+"""
+equity * stock weight = total market value in stock
+
+total market value in stock / 15 stocks = market value for each stock
+
+unused portion of stock = total market value in stock - current market value
+"""
