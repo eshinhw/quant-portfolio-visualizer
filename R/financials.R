4242341d1cfaadf43cfc7b3c94ddf9afb6982252
Eddie Shin <eshinhw@gmail.com>
2021-07-18 23:58:30 -0400

    removing outliers

--- R/financials.R
+++ R/financials.R
@@ -8,41 +8,65 @@ library(quantmod)
 library(tidyquant)
 library(PerformanceAnalytics)
 
+outlier_constant = 1.5
+
 fin <- read.csv('./data/financials.csv')
 
 glimpse(fin)
 
+remove_outliers <- function(factor) {
+  boxplot(factor)$stats
+  max_fence <- boxplot(factor)$stats[5,1]
+  min_fence <- boxplot(factor)$stats[1,1]
+  fin$DivYield <- ifelse(factor < min_fence
+                         | factor > max_fence, NA, factor)
+  fin <- fin %>% filter(!is.na(factor))
+  return(fin)
+}
+
 # remove outliers for dividend yield
-boxplot(fin$DivYield)$stats
-fin$DivYield <- ifelse(fin$DivYield < boxplot(fin$DivYield)$stats[1,1] 
-                       | fin$DivYield > boxplot(fin$DivYield)$stats[5,1], NA, fin$DivYield)
-fin <- fin %>% filter(!is.na(fin$DivYield))
+fin <- remove_outliers(fin$DivYield) 
+glimpse(fin)
+fin <- remove_outliers(fin$Revenue_Growth)
+glimpse(fin)
+fin <- remove_outliers(fin$DPS_Growth)
+fin <- remove_outliers(fin$ROE)
+fin <- remove_outliers(fin$GPMargin)
+
+glimpse(fin)
+# factor = fin$DivYield
+# boxplot(factor)$stats
+# threshold <- outlier_constant * (boxplot(factor)$stats[4,1] - boxplot(factor)$stats[2,1])
+# fin$DivYield <- ifelse(factor < boxplot(factor)$stats[2,1] - threshold 
+#                        | factor > boxplot(factor)$stats[4,1] + threshold, NA, factor)
+# fin <- fin %>% filter(!is.na(factor))
+
 
 # remove outliers for revenue growth
-boxplot(fin$Revenue_Growth)$stats
-fin$Revenue_Growth <- ifelse(fin$Revenue_Growth < boxplot(fin$Revenue_Growth)$stats[2,1], NA, fin$Revenue_Growth)
-fin <- fin %>% filter(!is.na(fin$Revenue_Growth))
-boxplot(fin$Revenue_Growth)
-
-# remove outliers for DPS growth
-boxplot(fin$DPS_Growth)$stats
-fin$DPS_Growth <- ifelse(fin$DPS_Growth < boxplot(fin$DPS_Growth)$stats[2,1] | fin$DPS_Growth > boxplot(fin$DPS_Growth)$stats[5,1], NA, fin$DPS_Growth)
-fin <- fin %>% filter(!is.na(fin$DPS_Growth))
-
-# remove outliers for EPS growth
-boxplot(fin$EPS_Growth)$stats
-fin$EPS_Growth <- ifelse(fin$EPS_Growth < boxplot(fin$EPS_Growth)$stats[3,1] | fin$EPS_Growth > boxplot(fin$EPS_Growth)$stats[5,1], NA, fin$EPS_Growth)
-fin <- fin %>% filter(!is.na(fin$EPS_Growth))
-
-# remove outliers for ROE
-boxplot(fin$ROE)$stats
-fin$ROE <- ifelse(fin$ROE < boxplot(fin$ROE)$stats[3,1], NA, fin$ROE)
-fin <- fin %>% filter(!is.na(fin$ROE))
-
-# remove outliers for Gross Profit Margin
-boxplot(fin$GPMargin)$stats
-fin$GPMargin <- ifelse(fin$GPMargin < boxplot(fin$GPMargin)$stats[2,1], NA, fin$GPMargin)
-fin <- fin %>% filter(!is.na(fin$GPMargin))
+# boxplot(fin$Revenue_Growth)$stats
+# fin$Revenue_Growth <- ifelse(fin$Revenue_Growth < boxplot(fin$Revenue_Growth)$stats[2,1], NA, fin$Revenue_Growth)
+# fin <- fin %>% filter(!is.na(fin$Revenue_Growth))
+# boxplot(fin$Revenue_Growth)
+# 
+# # remove outliers for DPS growth
+# boxplot(fin$DPS_Growth)$stats
+# fin$DPS_Growth <- ifelse(fin$DPS_Growth < boxplot(fin$DPS_Growth)$stats[2,1] | fin$DPS_Growth > boxplot(fin$DPS_Growth)$stats[5,1], NA, fin$DPS_Growth)
+# fin <- fin %>% filter(!is.na(fin$DPS_Growth))
+# 
+# # remove outliers for EPS growth
+# boxplot(fin$EPS_Growth)$stats
+# fin$EPS_Growth <- ifelse(fin$EPS_Growth < boxplot(fin$EPS_Growth)$stats[3,1] | fin$EPS_Growth > boxplot(fin$EPS_Growth)$stats[5,1], NA, fin$EPS_Growth)
+# fin <- fin %>% filter(!is.na(fin$EPS_Growth))
+# 
+# # remove outliers for ROE
+# boxplot(fin$ROE)$stats
+# fin$ROE <- ifelse(fin$ROE < boxplot(fin$ROE)$stats[3,1], NA, fin$ROE)
+# fin <- fin %>% filter(!is.na(fin$ROE))
+# 
+# # remove outliers for Gross Profit Margin
+# boxplot(fin$GPMargin)$stats
+# fin$GPMargin <- ifelse(fin$GPMargin < boxplot(fin$GPMargin)$stats[2,1], NA, fin$GPMargin)
+# fin <- fin %>% filter(!is.na(fin$GPMargin))
 
 # rank financial factors
 
