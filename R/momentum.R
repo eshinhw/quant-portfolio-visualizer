754e96a393cff322f86aa7e3d5f841b833a4fc7f
Eddie Shin <eshinhw@gmail.com>
2021-07-18 23:36:34 -0400

    momentum strategy

--- R/momentum.R
+++ R/momentum.R
@@ -19,7 +19,21 @@ tickers <- fin$symbol
 
 adj_prices <- map(tickers, GetMySymbols) %>% map(Ad) %>% reduce(merge.xts)
 
-ret <- Return.calculate(adj_prices)
+ret <- Return.calculate(adj_prices) %>% xts::last(252)
 
-ret_12m <- ret %>% xts::last(252)
-ret_12m
+ret_12m = ret %>% sapply(., function(x) {
+  prod(1+x) - 1
+})
+
+
+ret_12m[rank(-ret_12m) <= 15]
+
+invest_mom = rank(-ret_12m) <= 15
+invest_mom
+
+mom_stocks <- fin[invest_mom,] %>% select(symbol) %>% mutate('12M_return' = round(ret_12m[invest_mom], 4))
+
+glimpse(mom_stocks)
+
+mom_stocks
+
