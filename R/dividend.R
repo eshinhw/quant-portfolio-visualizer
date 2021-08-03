e1a7fdf540d70e27698b401387d09226a94b3c0a
Eddie Shin <eshinhw@gmail.com>
2021-08-03 11:38:20 -0400

    working on dividend factor

--- R/dividend.R
+++ R/dividend.R
@@ -14,6 +14,10 @@ fin <- read.csv('./data/dow_financials.csv')
 
 high_quality <- fin %>% filter(!is.na(DivYield)) %>% filter(Revenue_Growth > 0 & DPS_Growth > 0 & ROE > 0 & EPS_Growth > 0)
 
+div <- high_quality %>% select(symbol, name, DPS_Growth)
+
+div <- div %>% mutate(DPSGrowth_rank = rank(desc(DPS_Growth)))
+
 GetMySymbols <- function(x) {
   getSymbols(x, src='yahoo', from='2018-01-01', to=Sys.Date(), auto.assign=FALSE)
 }
@@ -30,6 +34,8 @@ adj_prices <- map(tickers, GetMySymbols) %>% map(Ad) %>% reduce(merge.xts)
 
 ret_12m <- Return.calculate(adj_prices) %>% xts::last(252) %>% 
   sapply(., function(x) {prod(1+x) - 1})
+
+ret_12m <- ret_12m %>% mutate(rank = rank(.)) %>% arrange(desc(rank))
 
 # ret_bind <- cbind(ret_3m, ret_6m, ret_12m) %>% data.frame()
 
