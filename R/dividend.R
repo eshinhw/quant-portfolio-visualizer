4175ae62498ba5c6439d566411b6fe0354324031
Eddie Shin <eshinhw@gmail.com>
2021-08-03 00:00:17 -0400

    quant investing process

--- R/dividend.R
+++ R/dividend.R
@@ -13,3 +13,16 @@ fin <- read.csv('./data/dow_financials.csv')
 # Dividend Stock + Remove negative revenue and dividend growth stocks
 
 high_quality <- fin %>% filter(!is.na(DivYield)) %>% filter(Revenue_Growth > 0 & DPS_Growth > 0 & ROE > 0 & EPS_Growth > 0)
+
+GetMySymbols <- function(x) {
+  getSymbols(x, src='yahoo', from='2018-01-01', to=Sys.Date(), auto.assign=FALSE)
+}
+
+tickers <- high_quality$symbol
+
+adj_prices <- map(tickers, GetMySymbols) %>% map(Ad) %>% reduce(merge.xts)
+
+ret_3m <- Return.calculate(adj_prices) %>% xts::last(60) %>% 
+  sapply(., function(x) {prod(1+x) - 1})
+
+ret_12m = ret 
