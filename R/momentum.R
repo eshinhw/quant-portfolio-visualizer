82c8bac0ac0699f14e780b8ff2bf82a04d79e762
Eddie Shin <eshinhw@gmail.com>
2021-08-13 21:15:12 -0400

    working on momentum

--- R/momentum.R
+++ R/momentum.R
@@ -19,11 +19,18 @@ tickers <- processed_fin$symbol
 
 adj_prices <- map(tickers, GetMySymbols) %>% map(Ad) %>% reduce(merge.xts)
 
-ret <- Return.calculate(adj_prices) %>% xts::last(252)
-
-ret_12m = ret %>% sapply(., function(x) {
+ret_6m = Return.calculate(adj_prices) %>% xts::last(126) %>% sapply(., function(x) {
   prod(1+x) - 1
 })
+
+ret_12m = Return.calculate(adj_prices) %>% xts::last(252) %>% sapply(., function(x) {
+  prod(1+x) - 1
+})
+
+ret_24m = Return.calculate(adj_prices) %>% xts::last(378) %>% sapply(., function(x) {
+  prod(1+x) - 1
+})
+
 
 
 ret_12m[rank(-ret_12m) <= 15]
