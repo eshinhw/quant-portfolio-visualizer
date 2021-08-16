5e724a91a1f594400dff3a0bd16544b4355cbe3d
Eddie Shin <eshinhw@gmail.com>
2021-08-16 01:13:12 -0400

    worked on quant analysis

--- analysis.py
+++ analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import utilities
 
 df = pd.read_csv('./R/data/dow_financials.csv')
 
@@ -10,6 +11,24 @@ conditions = (df['Revenue_Growth'] > 0) & \
             (df['ROE'] > 0) & \
             (df['DPS_Growth'] > 0) & \
             (df['DivYield'] > 0)
-
-
 df = df[conditions]
+
+# Compute historical momentum
+# Average momentum of prev 6M, 12M and 24M
+
+mom_list = []
+for symbol in df['symbol']:
+    print(symbol)
+    m6_momentum = utilities.calculate_hist_momentum(symbol, 120)
+    m12_momentum = utilities.calculate_hist_momentum(symbol, 252)
+    m24_momentum = utilities.calculate_hist_momentum(symbol, 504)
+    avg_momentum = (m6_momentum + m12_momentum + m24_momentum) / 3
+    mom_list.append(avg_momentum)
+
+df['momentum'] = mom_list
+print(df)
+
+df['mom_rank'] = df['momentum'].rank()
+
+print(df)
+
--- utilities.py
+++ utilities.py
@@ -1,17 +1,41 @@
 import smtplib
+import requests
 import credentials
 import numpy as np
 import pandas as pd
 import datetime as dt
 from typing import List
-import pandas_datareader.data as web
 from email.message import EmailMessage
 
 START_DATE = dt.datetime(1970, 1, 1)
 END_DATE = dt.datetime.today()
 
+def get_current_price(symbol):
+
+    data = requests.get(f"https://financialmodelingprep.com/api/v3/quote-short/{symbol}?apikey={credentials.FMP_API_KEYS}").json()
+    return data[0]['price']
+
 def get_daily_prices(symbol):
-    return web.DataReader(symbol, "yahoo", START_DATE, END_DATE)
+    data = requests.get(f"https://financialmodelingprep.com/api/v3/historical-price-full/{symbol}?apikey={credentials.FMP_API_KEYS}").json()
+    df_data = {'Date': [],
+               'Open': [],
+               'High': [],
+               'Low': [],
+               'Close': []}
+
+    for d in data['historical']:
+        df_data['Date'].append(d['date'])
+        df_data['Open'].append(d['open'])
+        df_data['High'].append(d['high'])
+        df_data['Low'].append(d['low'])
+        df_data['Close'].append(d['close'])
+
+    out_df = pd.DataFrame(df_data)
+    out_df = out_df[::-1]
+
+    out_df.set_index('Date', inplace=True)
+
+    return out_df
 
 def calculate_prev_min_low(symbol: str, period: int):
     df = get_daily_prices(symbol)
@@ -23,11 +47,18 @@ def calculate_prev_max_high(symbol: str, period: int):
     df["High_" + str(period)] = df["High"].shift(1).rolling(window=period).max()
     return df["High_" + str(period)].iloc[-1]
 
+def calculate_hist_momentum(symbol: str, period: int):
+    df = get_daily_prices(symbol)
+    currentPrice = df['Close'].iloc[-1]
+    prevPrice = df['Close'].iloc[-period]
+    ret = (currentPrice-prevPrice)/prevPrice
+    return ret
+
 def get_historical_monthly_prices(symbol: str):
     prices = get_daily_prices(symbol)
     prices.dropna(inplace=True)
     prices.reset_index(inplace=True)
-    prices = prices[["Date", "Adj Close"]]
+    prices = prices[["Date", "Close"]]
     prices["STD_YM"] = prices["Date"].map(lambda x: dt.datetime.strftime(x, "%Y-%m"))
     month_list = prices["STD_YM"].unique()
     monthly_prices = pd.DataFrame()
@@ -47,7 +78,6 @@ def calculate_momentum(symbol: str, periods: List[int]):
         mom = float(monthly_returns['Returns'][-1])
         if mom == np.nan:
             mom = float('-inf')
-        print(mom)
         ret.append(mom)
     return ret
 
