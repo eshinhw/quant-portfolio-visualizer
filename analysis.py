b3a0495abf7f5021cfb5e3149184ac83e440e68d
Eddie Shin <eshinhw@gmail.com>
2021-08-18 19:55:07 -0400

    updated analysis python file

--- analysis.py
+++ analysis.py
@@ -1,23 +1,52 @@
-import pandas as pd
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import dataSrc
 import utilities
-from dataSrc import sp500_symbols, dow_symbols, financials
+import pandas as pd
+
+
+# In[2]:
+
+
+# Retrieve symbols and financials
+
+sp500_tickers = dataSrc.sp500_symbols()
+dow_tickers = dataSrc.dow_symbols()
+
+
+# In[3]:
+
+
+# sp500_financials = dataSrc.financials(sp500_tickers)
+
+
+# In[4]:
+
+
+df = dataSrc.financials(dow_tickers)
+
+
+# In[6]:
+
 
 # df = pd.read_csv('./R/data/dow_financials.csv')
+# df = df.drop(['Unnamed: 0'], axis=1)
+# df = df.dropna()
 
-# sp500 = sp500_symbols()
-dowTickers = dow_symbols()
 
-df = financials(dowTickers)
+# In[7]:
 
-df = df.dropna()
 
-conditions = (df['Revenue_Growth'] > 0) & \
-            (df['GPMargin'] > 0)& \
-            (df['EPS_Growth'] > 0)& \
-            (df['ROE'] > 0) & \
-            (df['DPS_Growth'] > 0) & \
-            (df['DivYield'] > 0)
+conditions = (df['Revenue_Growth'] > 0) &             (df['GPMargin'] > 0)&             (df['EPS_Growth'] > 0)&             (df['ROE'] > 0) &             (df['DPS_Growth'] > 0) &             (df['DivYield'] > 0)
 df = df[conditions]
+
+
+# In[9]:
+
 
 # Compute historical momentum
 # Average momentum of prev 6M, 12M and 24M
@@ -25,16 +54,80 @@ df = df[conditions]
 mom_list = []
 for symbol in df['symbol']:
     print(symbol)
-    m6_momentum = utilities.calculate_hist_momentum(symbol, 120)
     m12_momentum = utilities.calculate_hist_momentum(symbol, 252)
     m24_momentum = utilities.calculate_hist_momentum(symbol, 504)
-    avg_momentum = (m6_momentum + m12_momentum + m24_momentum) / 3
+    m36_momentum = utilities.calculate_hist_momentum(symbol, 756)
+    avg_momentum = (m12_momentum + m24_momentum + m36_momentum) / 3
     mom_list.append(avg_momentum)
 
 df['momentum'] = mom_list
-print(df)
+df
+
+
+# In[17]:
+
 
 df['mom_rank'] = df['momentum'].rank()
+df = df.sort_values(by=['mom_rank'], ascending=False)
+# numRows = df.shape[0]
+# numCols = df.shape[1]
+# top10 = df.copy()
+df = df[df['momentum'] > 0]
 
-print(df)
+
+# In[18]:
+
+
+watchlist = df[['symbol','name']].copy()
+watchlist
+
+
+# In[19]:
+
+
+# Update current prices, 52W High and Discount %
+
+currentPrices = []
+highs = []
+discounts = []
+
+for symbol in watchlist['symbol']:
+    print(symbol)
+    currentPrice = utilities.get_current_price(symbol)
+    high = utilities.calculate_prev_max_high(symbol, 252)
+    discount_pct = (currentPrice - high) / high
+    
+    currentPrices.append(currentPrice)
+    highs.append(high)
+    discounts.append(discount_pct)
+    
+
+watchlist['CurrentPrice'] = currentPrices
+watchlist['52W_High'] = highs
+watchlist['Discount%'] = discounts
+    
+
+
+# In[20]:
+
+
+watchlist = watchlist.sort_values(by='Discount%')
+
+
+# In[21]:
+
+
+watchlist
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
