0627299356ce91b0353cbfbfc7b28d8f49f4ad32
Eddie Shin <eshinhw@gmail.com>
2021-10-08 21:00:50 -0400

    clean up before merge

--- questrade/questrade.py
+++ questrade/questrade.py
@@ -1,7 +1,7 @@
 import sys
 
 from os.path import dirname, abspath
-d = dirname(dirname(abspath(__file__))) # /home/kristina/desire-directory
+d = dirname(dirname(abspath(__file__)))
 sys.path.append(d)
 
 import datetime as dt
