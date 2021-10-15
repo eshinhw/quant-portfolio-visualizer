877f6f60c0e8554648e0149eb5dfde102dcb2877
Eddie Shin <eshinhw@gmail.com>
2021-10-14 21:41:18 -0400

    working on new oanda data processing functions

--- vol_breakout.py
+++ vol_breakout.py
@@ -1,8 +1,7 @@
 import json
-from demo_credentials import OANDA_API_KEY, VOL_BREAKOUT_ACCOUNT_ID
-# get a list of trades
 from oandapyV20 import API
 import oandapyV20.endpoints.trades as trades
+from demo_credentials import OANDA_API_KEY, VOL_BREAKOUT_ACCOUNT_ID
 
 api = API(access_token=OANDA_API_KEY)
 accountID = VOL_BREAKOUT_ACCOUNT_ID
@@ -12,3 +11,4 @@ r = trades.TradesList(accountID)
 print("REQUEST:{}".format(r))
 rv = api.request(r)
 print("RESPONSE:\n{}".format(json.dumps(rv, indent=2)))
+
