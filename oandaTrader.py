c5f40bb6c5c5f3ab6f7a76640c2f7de3a4f80105
Eddie Shin <eshinhw@gmail.com>
2021-11-08 18:05:01 -0500

    updated

--- oandaTrader.py
+++ oandaTrader.py
@@ -3,6 +3,7 @@ import pandas as pd
 from pprint import pprint
 from oandaData import OandaData
 from typing import List, Dict, Tuple
+from oandaInstruments import OandaInstrument
 import oandapyV20.endpoints.orders as orders
 import oandapyV20.endpoints.trades as trades
 # import oandapyV20.endpoints.pricing as pricing
@@ -11,7 +12,7 @@ import oandapyV20.endpoints.trades as trades
 from demo_credentials import OANDA_API_KEY, TEST_ACCOUNT_ID, VOLATILITY_BREAKOUT
 
 
-class OandaTrader(OandaData):
+class OandaTrader(OandaData, OandaInstrument):
 
     def __init__(self, api_key, accountID) -> None:
         super().__init__(api_key, accountID)
@@ -84,45 +85,45 @@ class OandaTrader(OandaData):
             return (unit_size, entry, stop, stop_loss_pips)
 
 
-    def fx_instruments(self):
-        quote = ['_USD','_CAD', '_JPY', '_CHF', '_SGD']
-        major = ['AUD_', 'USD_', 'NZD_', 'CAD_', 'EUR_', 'GBP_', 'SGD_']
-        quote_pairs = []
-        major_pairs = []
-        if os.name == "nt":
-            df = pd.read_csv('./instruments.csv')
-        if os.name == "posix":
-            df = pd.read_csv('/home/eshinhw/pyTrader/instruments.csv')
+    # def fx_instruments(self):
+    #     quote = ['_USD','_CAD', '_JPY', '_CHF', '_SGD']
+    #     major = ['AUD_', 'USD_', 'NZD_', 'CAD_', 'EUR_', 'GBP_', 'SGD_']
+    #     quote_pairs = []
+    #     major_pairs = []
+    #     if os.name == "nt":
+    #         df = pd.read_csv('./instruments.csv')
+    #     if os.name == "posix":
+    #         df = pd.read_csv('/home/eshinhw/pyTrader/instruments.csv')
 
-        df['Instrument'] = df['Instrument'].str.replace('/','_')
-        low_spread = df[df['Spread'] < 10].sort_values(by='Spread')
-        # print(low_spread.tail(10))
-        # print(low_spread)
-        # print(df['Instrument'])
-        for inst in low_spread['Instrument'].tolist():
-            for q in quote:
-                if q in inst:
-                    quote_pairs.append(inst)
+    #     df['Instrument'] = df['Instrument'].str.replace('/','_')
+    #     low_spread = df[df['Spread'] < 10].sort_values(by='Spread')
+    #     # print(low_spread.tail(10))
+    #     # print(low_spread)
+    #     # print(df['Instrument'])
+    #     for inst in low_spread['Instrument'].tolist():
+    #         for q in quote:
+    #             if q in inst:
+    #                 quote_pairs.append(inst)
 
-        for pair in quote_pairs:
-            for m in major:
-                if m in pair:
-                    major_pairs.append(pair)
-        return major_pairs
+    #     for pair in quote_pairs:
+    #         for m in major:
+    #             if m in pair:
+    #                 major_pairs.append(pair)
+    #     return major_pairs
 
-    def create_decimal_table(self):
-        trading_instruments = self.fx_instruments()
-        table = {}
-        for inst in trading_instruments:
-            if '_USD' in inst or '_CAD' in inst or '_CHF' in inst or '_SGD' in inst:
-                table[inst] = {}
-                table[inst]['decimal'] = 4
-                table[inst]['multiple'] = 10 ** 4
-            if '_JPY' in inst:
-                table[inst] = {}
-                table[inst]['decimal'] = 2
-                table[inst]['multiple'] = 10 ** 2
-        return table
+    # def create_decimal_table(self):
+    #     trading_instruments = self.fx_instruments()
+    #     table = {}
+    #     for inst in trading_instruments:
+    #         if '_USD' in inst or '_CAD' in inst or '_CHF' in inst or '_SGD' in inst:
+    #             table[inst] = {}
+    #             table[inst]['decimal'] = 4
+    #             table[inst]['multiple'] = 10 ** 4
+    #         if '_JPY' in inst:
+    #             table[inst] = {}
+    #             table[inst]['decimal'] = 2
+    #             table[inst]['multiple'] = 10 ** 2
+    #     return table
 
     def update_order_trade_status(self):
         trade_list = self.get_trade_list()
