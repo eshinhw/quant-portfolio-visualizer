b8045334a1fae2d296c159fd20dd7242c9719658
Eddie Shin <eshinhw@gmail.com>
2022-09-28 23:52:43 -0400

    page routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/login" element={<div>This is login page!</div>}/>
+        <Route path="/signup" />
+      </Routes>
     </div>
   );
 }
