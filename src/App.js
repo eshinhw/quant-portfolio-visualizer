dcdc94fed71393ca2ff6b6f433b7a3e53f4247c5
Eddie Shin <eshinhw@gmail.com>
2022-10-02 10:29:39 -0400

    implemented react Outlet

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, Outlet } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 // import HomePage from "./pages/Homepage.js";
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <HomePage />
       <Routes>
-        {/* <Route path="/" element={HomePage} /> */}
+        <Route path="/" element={HomePage} />
         <Route path="/login" element={<div>This is login page!</div>} />
         <Route path="/signup" />
+        <Route path="*" element={<div>404 Not Found</div>} />
+        <Route path="event" element={<Event />}>
+          <Route path="one" element={<div>First order free shipping!</div>}/>
+          <Route path="two" element={<div>Second order $100 coupon!</div>}/>
+        </Route>
       </Routes>
     </div>
+  );
+}
+
+function Event() {
+  return (
+    <>
+      <p>This is Event Page.</p>
+      <Outlet></Outlet>
+    </>
   );
 }
 
