4aa2d80427a9da92deec51ee29c1f2e0715a1c16
Eddie Shin <eshinhw@gmail.com>
2022-10-20 10:28:48 -0400

    created login and signup pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
 import React from "react";
 import { Routes, Route, Link, Outlet } from "react-router-dom";
-import Cart from "./components/Cart";
 import HomePage from "./pages/HomePage";
+import Login from "./pages/LoginPage";
+import Signup from "./pages/SignupPage";
 
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
       <Routes>
-        <Route path="/" element={HomePage} />
-        <Route path="/login" element={<div>This is login page!</div>} />
-        <Route path="/signup" />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />}/>
         <Route path="*" element={<div>404 Not Found</div>} />
         <Route path="event" element={<Event />}>
           <Route path="one" element={<div>First order free shipping!</div>}/>
           <Route path="two" element={<div>Second order $100 coupon!</div>}/>
         </Route>
-        <Route path="/cart" element={<Cart />}/>
       </Routes>
     </div>
   );
