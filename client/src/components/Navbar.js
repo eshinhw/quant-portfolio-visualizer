2cc928aad8c7325167b2d68cf2e9a028a7461bcd
Eddie Shin <eshinhw@gmail.com>
2023-01-26 12:08:16 -0500

    worked on Navbar components

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,7 +9,7 @@ export default function NavBar() {
         <Container>
           <Navbar.Brand href="/">Trading Automated</Navbar.Brand>
           <Nav className="ml-auto">
-            <Nav.Link href="/sector-etf">Sector ETFs</Nav.Link>
+            <Nav.Link href="/sector-etf">Sector Analysis</Nav.Link>
             <Nav.Link href="/price-alert">Price Alert</Nav.Link>
             <Nav.Link href="/screener">Screener</Nav.Link>
             <Nav.Link href="/position-calculator">Position Calculator</Nav.Link>
