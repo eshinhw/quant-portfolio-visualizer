d0a499195b05fa47e6a018d807feca5ff98c2dff
Eddie Shin <eshinhw@gmail.com>
2023-01-27 23:48:51 -0500

    working on backend financial data

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const { historicalPriceData } = require("./FinancialData");
 const app = express();
 const port = 3000;
+
+let quotes = historicalPriceData("AAPL");
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
