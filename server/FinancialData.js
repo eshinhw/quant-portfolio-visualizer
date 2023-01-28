f520076e243cc5f975bf3578d60a648f4fb31a74
Eddie Shin <eshinhw@gmail.com>
2023-01-27 23:48:53 -0500

    working on backend financial data

--- server/FinancialData.js
+++ server/FinancialData.js
@@ -1,12 +1,17 @@
 const yahooFinance = require("yahoo-finance");
 
-yahooFinance.historical(
-  {
-    symbol: "AAPL",
-    from: "2020-01-01",
-    to: "2021-10-10",
-  },
-  function (err, quotes) {
-    console.log(quotes);
-  }
-);
+async function historicalPriceData(symbol) {
+  yahooFinance.historical(
+    {
+      symbol: "AAPL",
+      from: "2020-01-01",
+      to: "2021-10-10",
+    },
+    function (err, quotes) {
+      // console.log(quotes);
+      return quotes;
+    }
+  );
+}
+
+module.exports = { historicalPriceData };
