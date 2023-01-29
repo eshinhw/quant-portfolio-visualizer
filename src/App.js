7a7a51da593e8560cf48ea4bd071f3aa42d06ecb
Eddie Shin <eshinhw@gmail.com>
2023-01-29 17:24:19 -0500

    initialized robinhood-clone project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='app__header'>
+        
+      </div>
+      <div className='app__body'>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
