152903a8d563f7ae2b850dc0f8e787be743d51eb
Eddie Shin <eshinhw@gmail.com>
2023-01-29 18:20:32 -0500

    worked on navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <div className='app__header'>
-        
+      <div className="app__header">
+        <Header />
       </div>
-      <div className='app__body'>
-
-      </div>
+      <div className="app__body"></div>
     </div>
   );
 }
