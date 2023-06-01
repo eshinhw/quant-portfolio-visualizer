6c95412a4d4345395ceaee603896479557926aeb
Eddie Shin <eshinhw@gmail.com>
2023-06-01 00:01:12 -0400

    feat: set up inital Flask backend

---
+++ flask-server/server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Members API Route
+
+@app.route("/members")
+def members():
+    return {"memebrs": ["Member1", "Member2", "Member3"]}
+
+if __name__ == "__main__":
+    app.run(debug=True)
