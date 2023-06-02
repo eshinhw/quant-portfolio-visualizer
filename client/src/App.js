448478fcbe63cdc8a973f5eff750693c9fb027ed
Eddie Shin <eshinhw@gmail.com>
2023-06-01 23:04:47 -0400

    tested flask backend with react frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([{}]);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("/members")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+        setLoaded(true);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {!loaded ? (
+        <p>Loading...</p>
+      ) : (
+        data.members.map((d, i) => <p key={i}>{d}</p>)
+      )}
     </div>
   );
 }
--- flask-server/server.py
+++ flask-server/server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/members")
 def members():
-    return {"memebrs": ["Member1", "Member2", "Member3"]}
+    return {"members": ["Member1", "Member2", "Member3"]}
 
 if __name__ == "__main__":
     app.run(debug=True)
