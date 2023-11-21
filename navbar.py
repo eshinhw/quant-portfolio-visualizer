5b8f181202b36986270e66fa964868701ba4b739
Eddie Shin <eshinhw@gmail.com>
2023-11-20 23:01:50 -0500

    Create new navbar

---
+++ navbar.py
@@ -0,0 +1,40 @@
+import dash_bootstrap_components as dbc
+
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.DropdownMenu(
+            children=[
+                dbc.DropdownMenuItem("More pages", header=True),
+                dbc.DropdownMenuItem("Page 2", href="#"),
+                dbc.DropdownMenuItem("Page 3", href="#"),
+            ],
+            nav=True,
+            in_navbar=True,
+            label="Fama-French Factors",
+        ),
+        dbc.DropdownMenu(
+            children=[
+                dbc.DropdownMenuItem("More pages", header=True),
+                dbc.DropdownMenuItem("Page 2", href="#"),
+                dbc.DropdownMenuItem("Page 3", href="#"),
+            ],
+            nav=True,
+            in_navbar=True,
+            label="Fixed Portfolios",
+        ),
+        dbc.DropdownMenu(
+            children=[
+                dbc.DropdownMenuItem("More pages", header=True),
+                dbc.DropdownMenuItem("Page 2", href="#"),
+                dbc.DropdownMenuItem("Page 3", href="#"),
+            ],
+            nav=True,
+            in_navbar=True,
+            label="Dynamic Asset Allocations",
+        ),
+    ],
+    brand="Quant Portfolio Visualizer",
+    brand_href="/",
+    color="primary",
+    dark=True,
+)
