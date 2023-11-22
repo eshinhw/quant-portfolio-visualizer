8eae55bafd917b0231c054a6d0a2b654b76efb1c
Eddie Shin <eshinhw@gmail.com>
2023-11-21 22:16:37 -0500

    Add size factor

---
+++ factors/size.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+import pandas_datareader.data as web
+
+QUINTILES = ["Lo 20", "Qnt 2", "Qnt 3", "Qnt 4", "Hi 20"]
+
+
+def get_size_cummulative_returns():
+    monthly_ret = web.DataReader(
+        "Portfolios_Formed_on_ME", "famafrench", start="1900-01-01"
+    )[0]
+    monthly_ret_cum = np.log(1 + monthly_ret / 100).cumsum()
+    monthly_ret_cum.reset_index(inplace=True)
+    monthly_ret_cum["Date"] = monthly_ret_cum["Date"].astype(str)
+    monthly_ret_cum.rename(columns={"Lo 20": "SMALL", "Hi 20": "BIG"}, inplace=True)
+    # print(monthly_ret_cum)
+    return monthly_ret_cum[["Date", "SMALL", "BIG"]]
+
+
+def size_factor_stat():
+    data = {}
+
+    monthly_ret = web.DataReader(
+        "Portfolios_Formed_on_ME", "famafrench", start="1900-01-01"
+    )[0][QUINTILES]
+    n = len(monthly_ret)
+    ret_ari = (monthly_ret / 100).mean(axis=0) * 12
+    ret_geo = (1 + monthly_ret / 100).prod() ** (12 / n) - 1
+    vol = (monthly_ret / 100).std(axis=0) * np.sqrt(12)
+    sharpe = ret_ari / vol
+
+    data["Quintiles"] = QUINTILES
+    data["Arithmetic Mean"] = list(ret_ari.values)
+    data["Geometric Mean"] = list(ret_geo.values)
+    data["Annualized Volatility"] = list(vol)
+    data["Sharpe Ratio"] = list(sharpe)
+
+    stat = pd.DataFrame(data).round(3)
+
+    return stat
+
+
+if __name__ == "__main__":
+    # get_beta_cummulative_returns()
+    pass
---
+++ pages/size.py
@@ -0,0 +1,39 @@
+import dash
+from dash import Dash, html, dcc, callback, Output, Input, dash_table, ctx
+from factors.size import get_size_cummulative_returns, size_factor_stat
+import plotly.express as px
+
+
+dash.register_page(__name__)
+
+
+def update_graph():
+    df = get_size_cummulative_returns()
+    fig = px.line(df, x="Date", y=[col for col in df.columns if col != "Date"])
+    return fig
+
+
+def update_table():
+    return size_factor_stat().to_dict("records")
+
+
+def update_heading():
+    df = get_size_cummulative_returns()
+
+    startDate = df["Date"].iloc[0].replace("-", ".")
+    endDate = df["Date"].iloc[-1].replace("-", ".")
+    return html.H3(
+        f"Size Factor ({startDate} ~ {endDate})", className="text-center m-2"
+    )
+
+
+layout = html.Div(
+    [
+        update_heading(),
+        dcc.Graph(id="graph-content", figure=update_graph()),
+        dash_table.DataTable(
+            id="table-content", sort_action="native", data=update_table()
+        ),
+    ],
+    className="m-2",
+)
