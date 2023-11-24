8c8d6383d630630ff32e303f49948198e77ddadb
Eddie Shin <eshinhw@gmail.com>
2023-11-24 00:08:56 -0500

    Create homepage

---
+++ pages/home.py
@@ -0,0 +1,42 @@
+import dash
+from dash import Dash, html, dcc, callback, Output, Input, dash_table, ctx
+
+import plotly.express as px
+
+
+dash.register_page(__name__, path="/")
+
+
+layout = html.Div(
+    [
+        html.Div(
+            [
+                html.H5("Fama-French Factor Analysis"),
+                html.Ul(
+                    [
+                        html.Li("Market Beta"),
+                        html.Li("Size"),
+                        html.Li("Value"),
+                        html.Li("Momentum"),
+                    ]
+                ),
+                html.H5("Fixed Portfolios"),
+                html.Ul(
+                    [
+                        html.Li("Classic 60/40 Portfolio"),
+                        html.Li("All Weather Portfolio"),
+                        html.Li("Permanent Portfolio"),
+                    ]
+                ),
+                html.H5("Dynamic Portfolios"),
+            ],
+            style={
+                "margin-left": "60px",
+                "padding-left": "12px",
+                # "margin-right": "56px",
+                "margin-top": "20px",
+                "max-width": "1320px",
+            },
+        ),
+    ],
+)
