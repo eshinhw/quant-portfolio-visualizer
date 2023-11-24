ad0a456e311cee4c71fde169ed5335b9e3f521dd
Eddie Shin <eshinhw@gmail.com>
2023-11-24 11:33:46 -0500

    Update app.py which is fully functioning

--- app.py
+++ app.py
@@ -13,22 +13,6 @@ app = Dash(
 app.layout = html.Div(
     [
         navbar,
-        # html.Div(
-        #     [
-        #         html.H3("Fama-French Factor Analysis"),
-        #         html.Ul(
-        #             [
-        #                 html.Li("Market Beta"),
-        #                 html.Li("Size"),
-        #                 html.Li("Value"),
-        #                 html.Li("Momentum"),
-        #             ]
-        #         ),
-        #         html.H3("Fixed Portfolios"),
-        #         html.H3("Dynamic Portfolios"),
-        #     ],
-        #     style={"margin-left": "10px", "margin-right": "10px"},
-        # ),
         dash.page_container,
     ],
 )
