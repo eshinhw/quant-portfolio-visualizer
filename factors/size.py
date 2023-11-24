1200878739d16281f6e515ece81d57efdf151c0d
Eddie Shin <eshinhw@gmail.com>
2023-11-24 11:34:10 -0500

    Complete fama-french size factor

--- factors/size.py
+++ factors/size.py
@@ -6,9 +6,10 @@ QUINTILES = ["Lo 20", "Qnt 2", "Qnt 3", "Qnt 4", "Hi 20"]
 
 
 def get_size_cummulative_returns():
-    monthly_ret = web.DataReader(
-        "Portfolios_Formed_on_ME", "famafrench", start="1900-01-01"
-    )[0]
+    monthly_ret = pd.read_csv("./famafrench/Portfolios_Formed_on_ME.csv", skiprows=11)
+    monthly_ret[["Lo 20", "Qnt 2", "Qnt 3", "Qnt 4", "Hi 20"]]
+    monthly_ret["Date"] = pd.to_datetime(monthly_ret["Date"], format="%Y%m")
+    monthly_ret.set_index("Date", inplace=True)
     monthly_ret_cum = np.log(1 + monthly_ret / 100).cumsum()
     monthly_ret_cum.reset_index(inplace=True)
     monthly_ret_cum["Date"] = monthly_ret_cum["Date"].astype(str)
@@ -20,16 +21,15 @@ def get_size_cummulative_returns():
 def size_factor_stat():
     data = {}
 
-    monthly_ret = web.DataReader(
-        "Portfolios_Formed_on_ME", "famafrench", start="1900-01-01"
-    )[0][QUINTILES]
+    monthly_ret = get_size_cummulative_returns()
+    monthly_ret.set_index("Date", inplace=True)
     n = len(monthly_ret)
     ret_ari = (monthly_ret / 100).mean(axis=0) * 12
     ret_geo = (1 + monthly_ret / 100).prod() ** (12 / n) - 1
     vol = (monthly_ret / 100).std(axis=0) * np.sqrt(12)
     sharpe = ret_ari / vol
 
-    data["Quintiles"] = QUINTILES
+    data["Quintiles"] = ["SMALL", "BIG"]
     data["Arithmetic Mean"] = list(ret_ari.values)
     data["Geometric Mean"] = list(ret_geo.values)
     data["Annualized Volatility"] = list(vol)
