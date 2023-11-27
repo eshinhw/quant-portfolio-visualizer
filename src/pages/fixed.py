284e693a097b1ebc24b7af6ff58b7b06839ddbcb
Eddie Shin <eshinhw@gmail.com>
2023-11-27 12:49:33 -0500

    Make page responsive and update details

--- src/pages/fixed.py
+++ src/pages/fixed.py
@@ -4,6 +4,7 @@ from strategies.BasePortfolio import BasePortfolio
 from dash import Dash, html, dcc, callback, Output, Input, dash_table, ctx
 import plotly.express as px
 import pandas as pd
+import dash_bootstrap_components as dbc
 
 dash.register_page(__name__, title="Quant Portfolio Visualizer")
 
@@ -42,7 +43,6 @@ def update_returns_graph():
         df,
         x="Date",
         y=[col for col in df.columns if col != "Date"],
-        title="Historical Returns",
     )
 
 
@@ -58,27 +58,40 @@ def update_drawdown_graph():
         df,
         x="Date",
         y=[col for col in df.columns if col != "Date"],
-        title="Historical Drawdowns",
     )
 
 
-layout = html.Div(
+layout = dbc.Container(
     [
         html.Div(
             [
-                dcc.Graph(figure=update_returns_graph(), id="total-perf-graph"),
-                dcc.Graph(figure=update_drawdown_graph(), id="total-drawdown-graph"),
-                html.Div(
-                    [
-                        dash_table.DataTable(
-                            update_table(),
-                            id="total-perf-table",
-                            style_cell={"textAlign": "center"},
-                        )
-                    ]
+                html.H5("Fixed Portfolios Summary", style={"margin-bottom": "20px"}),
+                dash_table.DataTable(
+                    update_table(),
+                    id="total-perf-table",
+                    style_cell={"textAlign": "center"},
+                    style_table={"overflowX": "auto"},
+                    sort_action="native",
                 ),
-            ],
-            style={"margin": "0px 35px 0px 35px"},
+            ]
+        ),
+        html.Div(
+            [
+                html.H5("Historical Cummulative Returns", style={"margin-top": "20px"}),
+                dcc.Graph(
+                    figure=update_returns_graph(),
+                    id="total-perf-graph",
+                    responsive=True,
+                ),
+            ]
+        ),
+        html.Div(
+            [
+                html.H5("Historical Drawdowns"),
+                dcc.Graph(figure=update_drawdown_graph(), id="total-drawdown-graph"),
+            ]
         ),
     ],
+    fluid="md",
+    className="content-container",
 )
