954fb198f398e9d572a33401d21539501c75b145
Eddie Shin <eshinhw@gmail.com>
2024-03-08 13:05:23 -0500

    Remove fama-franch factor page

--- src/navbar.py
+++ src/navbar.py
@@ -2,17 +2,6 @@ import dash_bootstrap_components as dbc
 
 navbar = dbc.NavbarSimple(
     children=[
-        dbc.DropdownMenu(
-            children=[
-                dbc.DropdownMenuItem("Market Beta", href="/beta"),
-                dbc.DropdownMenuItem("Size", href="/size"),
-                dbc.DropdownMenuItem("Value", href="/value"),
-                dbc.DropdownMenuItem("Momentum", href="/momentum"),
-            ],
-            nav=True,
-            in_navbar=True,
-            label="Fama-French Factors",
-        ),
         dbc.NavItem(dbc.NavLink("Fixed Portfolios", href="/fixed")),
         dbc.DropdownMenu(
             children=[
